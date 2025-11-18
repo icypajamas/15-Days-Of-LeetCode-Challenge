3b1664c5e3259c639d80d4a5c2799f271b64a54f
icypajamas <ch.hishamabbas@gmail.com>
2025-11-18 22:38:39 +0500

    Time: 2 ms (14.01%) | Memory: 46.9 MB (9.78%) - LeetSync

--- 412-fizz-buzz/fizz-buzz.java
+++ 412-fizz-buzz/fizz-buzz.java
@@ -1,19 +1,16 @@
 class Solution {
     public List<String> fizzBuzz(int n) {
         String[] answer = new String[n];
-        for(int i = 0; i < n; i++){  // Start from 0
-            int number = i + 1;  // Convert index to actual number
-            
-            if(number % 3 == 0 && number % 5 == 0){
-                answer[i] = "FizzBuzz";
-            }else if(number % 3 == 0){
-                answer[i] = "Fizz";
-            }else if(number % 5 == 0){
-                answer[i] = "Buzz";
-            }else {
-                answer[i] = String.valueOf(number);
-            }
+        for(int i = 1; i <= answer.length; i++){
+            if(i % 3 == 0 && i % 5 == 0){
+                answer[i-1] = "FizzBuzz";
+            }else if(i % 3 == 0){
+                answer[i-1] = "Fizz";
+            }else if(i % 5 == 0){
+                answer[i-1] = "Buzz";
+            }else answer[i-1] = ""+i;
         }
         return Arrays.asList(answer);
+
     }
 }
