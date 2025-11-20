94a7d128ac45fe5628454d369e4e22425babeeaa
icypajamas <ch.hishamabbas@gmail.com>
2025-11-20 23:39:01 +0500

    Time: 8 ms (9.31%) | Memory: 45.8 MB (17.96%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,17 +1,7 @@
 class Solution {
     public boolean isPalindrome(int x) {
         String str = String.valueOf(x);
-        char[] org = str.toCharArray();
-        int left = 0;
-        int right = org.length - 1;
-
-        for(int i = 0; i < str.length(); i++){
-            if(org[left] != org[right]){
-                return false;
-            }
-            right --;
-            left ++;
-        }
-        return true;
+        String reversed = new StringBuilder(str).reverse().toString();
+    return str.equals(reversed);
     }
 }
